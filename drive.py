806a729a46b5973ec9ee69f1eb6afe95d8af7e4d
Matthias Ludwig <m.ludwig@klebert-engineering.de>
2017-04-13 15:01:58 +0200

    disable break of pi-controller

--- drive.py
+++ drive.py
@@ -65,6 +65,8 @@ def telemetry(sid, data):
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
+        if throttle < 0:
+          throttle = 0
 
         print(steering_angle, throttle)
         send_control(steering_angle, throttle)
