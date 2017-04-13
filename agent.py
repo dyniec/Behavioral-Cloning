f61a1f5208a4cdc157a84ecad85f057416d11fe4
Matthias Ludwig <m.ludwig@klebert-engineering.de>
2017-04-13 14:58:58 +0200

    Use generator for training

--- agent.py
+++ agent.py
@@ -5,24 +5,30 @@ from cv2 import imread, flip, cvtColor, COLOR_BGR2HSV
 import numpy as np
 import random
 
-def apply_jitter(values, max_jitter=0.25):
-  for i in range(len(values)):
-    values[i] = values[i] - max_jitter + max_jitter * 2 * random.random()
-  return values
+def apply_jitter(value, max_jitter=0.1):
+  return value - max_jitter + max_jitter * 2 * random.random()
 
 def load_data(sample_dir):
-  images = []
-  steering = []
+  samples = []
   with open(sample_dir + '/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
+      samples.append(line)
+  return samples
+
+def generate_batch(samples, sample_dir):
+  from sklearn.utils import shuffle
+  while 1:
+    for line in shuffle(samples):
+      images = []
+      steering = []
       img_center = imread(sample_dir + '/IMG/' + line[0].split('/')[-1])
       img_center = cvtColor(img_center, COLOR_BGR2HSV)
       img_left = imread(sample_dir + '/IMG/' + line[1].split('/')[-1])
       img_left = cvtColor(img_left, COLOR_BGR2HSV)
       img_right = imread(sample_dir + '/IMG/' + line[2].split('/')[-1])
       img_right = cvtColor(img_right, COLOR_BGR2HSV)
-      angle = float(line[3])
+      angle = apply_jitter(float(line[3]))
 
       images.append(img_center)
       images.append(flip(img_center, 1))
@@ -33,16 +39,13 @@ def load_data(sample_dir):
       steering.append(-angle)
       steering.append(angle + 0.6)
       steering.append(angle - 0.6)
-  return (np.array(images), np.array(steering))
-
-images, steering_angles = load_data('data')
-print('Loaded {} samples'.format(len(steering_angles)))
+      yield (np.array(images), np.array(steering))
 
 import keras
 
 def create_model():
   model = keras.models.Sequential()
-  model.add(keras.layers.convolutional.Cropping2D(cropping=((80, 20), (0, 0)), input_shape=images[0].shape))
+  model.add(keras.layers.convolutional.Cropping2D(cropping=((80, 20), (0, 0)), input_shape=(160,320,3)))
   model.add(keras.layers.Lambda(lambda x: x / 255.0 - 0.5))
   model.add(keras.layers.Conv2D(10, (5, 5), activation='relu'))
   model.add(keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
@@ -70,6 +73,7 @@ if __name__ == '__main__':
   import argparse
   parser = argparse.ArgumentParser(description='Agent training')
   parser.add_argument('--load', type=str, required=False, default=None, help='filename to load model from')
+  parser.add_argument('--data', type=str, required=False, default='data', help='path to training data')
   args = parser.parse_args()
 
   model = None
@@ -80,5 +84,18 @@ if __name__ == '__main__':
     print("creating new model")
     model = create_model()
 
-  model.fit(images, apply_jitter(steering_angles), validation_split=0.2, epochs=3, shuffle=True)
+  data = 'data'
+  if args.data:
+    data = args.data
+
+  print("Loading training data from", data)
+  samples = load_data(data)
+  print('Loaded {} samples'.format(len(samples)))
+
+  from sklearn.model_selection import train_test_split
+  train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+  train_generator = generate_batch(train_samples, data)
+  validation_generator = generate_batch(validation_samples, data)
+
+  model.fit_generator(train_generator, len(train_samples), validation_data=validation_generator, validation_steps = len(validation_samples), epochs = 3)
   model.save('model.h5')
