12656d55b040f9b4832342852e1b9da6735ed152
Matthias Ludwig <m.ludwig@klebert-engineering.de>
2017-04-15 18:29:09 +0200

    Improve model & training
    * Add linear activation to flat layers
    * Jitter all samples
    * Randomly darken samples
    * Remove validation for faster training
    * Rename to model.py as requested

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from cv2 import imread, flip, cvtColor, COLOR_BGR2HSV
 import numpy as np
 import random
 
-def apply_jitter(value, max_jitter=0.1):
+def apply_jitter(value, max_jitter=0.05):
   return value - max_jitter + max_jitter * 2 * random.random()
 
 def load_data(sample_dir):
@@ -16,19 +16,25 @@ def load_data(sample_dir):
       samples.append(line)
   return samples
 
-def generate_batch(samples, sample_dir):
+def load_and_optimize(path):
+  image = imread(path)
+  image = cvtColor(image, COLOR_BGR2HSV)
+  if (random.random() > 0.5):
+    image[:,:,2] = image[:,:,2] * .25+np.random.uniform()
+  return image
+
+def generate_batch(samples, sample_dir, batch_size=32):
   from sklearn.utils import shuffle
   while 1:
+    images = []
+    steering = []
     for line in shuffle(samples):
-      images = []
-      steering = []
-      img_center = imread(sample_dir + '/IMG/' + line[0].split('/')[-1])
-      img_center = cvtColor(img_center, COLOR_BGR2HSV)
-      img_left = imread(sample_dir + '/IMG/' + line[1].split('/')[-1])
-      img_left = cvtColor(img_left, COLOR_BGR2HSV)
-      img_right = imread(sample_dir + '/IMG/' + line[2].split('/')[-1])
-      img_right = cvtColor(img_right, COLOR_BGR2HSV)
-      angle = apply_jitter(float(line[3]))
+      img_center = load_and_optimize(sample_dir + '/IMG/' + line[0].split('/')[-1])
+      img_left = load_and_optimize(sample_dir + '/IMG/' + line[1].split('/')[-1])
+      img_right = load_and_optimize(sample_dir + '/IMG/' + line[2].split('/')[-1])
+      angle = float(line[3])
+
+      angle = apply_jitter(angle)
 
       images.append(img_center)
       images.append(flip(img_center, 1))
@@ -37,11 +43,13 @@ def generate_batch(samples, sample_dir):
 
       steering.append(angle)
       steering.append(-angle)
-      steering.append(angle + 0.6)
-      steering.append(angle - 0.6)
-      # only use few of the samples without stearing
-      if (((angle < -0.1) or (angle > 0.1)) and random.random() > 0.9):
-        yield (np.array(images), np.array(steering))
+      steering.append(min(1.0, angle + 0.3))
+      steering.append(max(-1.0, angle - 0.3))
+
+      if (len(images) >= batch_size):
+        yield shuffle(np.array(images), np.array(steering))
+        images = []
+        steering = []
 
 import keras
 
@@ -60,11 +68,11 @@ def create_model():
 
   model.add(keras.layers.Flatten())
 
-  model.add(keras.layers.Dense(30))
+  model.add(keras.layers.Dense(30, activation='tanh'))
   model.add(keras.layers.Dropout(0.2))
-  model.add(keras.layers.Dense(20))
+  model.add(keras.layers.Dense(20, activation='tanh'))
   model.add(keras.layers.Dropout(0.2))
-  model.add(keras.layers.Dense(10))
+  model.add(keras.layers.Dense(10, activation='tanh'))
   model.add(keras.layers.Dropout(0.2))
   model.add(keras.layers.Dense(1))
 
@@ -76,6 +84,7 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Agent training')
   parser.add_argument('--load', type=str, required=False, default=None, help='filename to load model from')
   parser.add_argument('--data', type=str, required=False, default='data', help='path to training data')
+  parser.add_argument('--epochs', type=int, required=False, default=10, help='number of epochs')
   args = parser.parse_args()
 
   model = None
@@ -94,10 +103,8 @@ if __name__ == '__main__':
   samples = load_data(data)
   print('Loaded {} samples'.format(len(samples)))
 
-  from sklearn.model_selection import train_test_split
-  train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-  train_generator = generate_batch(train_samples, data)
-  validation_generator = generate_batch(validation_samples, data)
+  train_samples = samples
+  train_generator = generate_batch(train_samples, data, batch_size=32)
 
-  model.fit_generator(train_generator, len(train_samples), validation_data=validation_generator, validation_steps = len(validation_samples), epochs = 3)
+  model.fit_generator(train_generator, len(train_samples) * 4 / 32, epochs = args.epochs)
   model.save('model.h5')
